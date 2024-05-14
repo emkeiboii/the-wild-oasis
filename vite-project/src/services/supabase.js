4027ba2586d8f6ea79c38c8e9acfecb13b2dc2f8
Mark <marklajom.work@gmail.com>
2024-05-14 16:12:02 +0200

    add connection supabase to react

---
+++ vite-project/src/services/supabase.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://upwozmhtthvdetztnxnw.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwd296bWh0dGh2ZGV0enRueG53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MDgyOTcsImV4cCI6MjAzMTE4NDI5N30.W6WJ41NJGamtCj-5lzkth-tAaktIijeNwX0kDy3xe7E";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
