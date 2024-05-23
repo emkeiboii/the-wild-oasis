d86e46f0cdaab642ebb69e8ebcb1f9f28618eeee
Mark <marklajom.work@gmail.com>
2024-05-23 11:38:12 +0200

    add notifications with react toast

--- vite-project/src/features/cabins/CabinRow.jsx
+++ vite-project/src/features/cabins/CabinRow.jsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -58,12 +59,10 @@ function CabinRow({ cabin }) {
   const { isLoading, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert("Cabin succesfully deleted");
+      toast.success("Correctly deleted cabin");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
     },
-    onError: () => {
-      (err) => alert(err.message);
-    },
+    onError: (err) => toast.error(err.message),
   });
 
   const queryClient = useQueryClient();
