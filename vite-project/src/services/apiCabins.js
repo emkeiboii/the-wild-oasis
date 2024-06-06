e27a8b28d2f838a62563e69e30dfba8a59c5691e
Mark <marklajom.work@gmail.com>
2024-06-06 16:20:23 +0200

    add handle error form and upload image

--- vite-project/src/services/apiCabins.js
+++ vite-project/src/services/apiCabins.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
-import supabase from "./supabase";
+import { MdDescription } from "react-icons/md";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -15,6 +16,38 @@ export async function deleteCabin(id) {
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be deleted");
+  }
+
+  return data;
+}
+
+// addCabin
+export async function addCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replace("/", "");
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }])
+    .select();
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+  }
+
+  // upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  //delete cabin if error uploading
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Image could not be uploaded and the cabin could not be created"
+    );
   }
   return data;
 }
