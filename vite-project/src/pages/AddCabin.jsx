9f6032d983340aa83ba5b32642e641b2537c0962
Mark <marklajom.work@gmail.com>
2024-07-05 15:30:15 +0200

    update modal to compount component

--- vite-project/src/pages/AddCabin.jsx
+++ vite-project/src/pages/AddCabin.jsx
@@ -1,22 +1,24 @@
-import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 import Button from "../ui/Button";
 import Modal from "../ui/Modal";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-
+import CabinTable from "../features/cabins/CabinTable";
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="cabin-table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
 
