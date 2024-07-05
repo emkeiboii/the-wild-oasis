1efbbf87ee45b9dbb791d7038125f25ed7660ba9
Mark <marklajom.work@gmail.com>
2024-07-05 17:05:37 +0200

    add outside click to close hook

--- vite-project/src/pages/Cabins.jsx
+++ vite-project/src/pages/Cabins.jsx
@@ -1,18 +1,9 @@
-/* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import Button from "../ui/Button";
 import AddCabin from "./addCabin";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
     <>
       <Row type="horizontal">
--- vite-project/src/ui/Modal.jsx
+++ vite-project/src/ui/Modal.jsx
@@ -3,6 +3,7 @@ import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -52,7 +53,6 @@ const Button = styled.button`
     color: var(--color-grey-500);
   }
 `;
-
 const ModalContext = createContext();
 
 function Modal({ children }) {
@@ -76,12 +76,13 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
 
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
