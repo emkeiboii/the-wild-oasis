edd71ebcd5fab8fc80ca8deb09d6e460e906f842
Mark <marklajom.work@gmail.com>
2024-07-23 09:10:03 +0200

    fix bug in useLogin hook

--- vite-project/src/features/authentication/useLogin.js
+++ vite-project/src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     // cache the new login data
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
